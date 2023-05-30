86bea66b1c32d332480b48eb24240d281681ad21
calvin <ncalvin67@gmail.com>
2023-05-30 20:33:27 +0300

    Fetch list from test-data

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from "cors";
 
 import testData from "../test-data.json";
 
 const router = express.Router();
+router.use(cors());
 
 router.get("/contests", (req, res) => {
     // get the data from MongoDB
